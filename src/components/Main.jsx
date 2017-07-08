c8998baae4b2160cb023c6250a6735a6a8f449de
Amber Murray <amber.applebee@gmail.com>
2017-07-08 13:32:11 -0700

    added react-ace-editor

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import { Switch, Route } from 'react-router-dom'
-import '../styles/main.css'
 import Home from './Home'
 import Animations from './Animations'
 import Buttons from './Buttons'
