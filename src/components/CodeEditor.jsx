a90eedc7a5956035fe8a220cd47f0e78e359d316
Amber Murray <amber.applebee@gmail.com>
2017-07-09 11:52:32 -0700

    adjusting AceEditor settings

--- src/components/CodeEditor.jsx
+++ src/components/CodeEditor.jsx
@@ -1,28 +1,33 @@
 import React, { Component } from 'react';
 import AceEditor from 'react-ace';
 
-import 'brace/mode/javascript';
+import 'brace/mode/css';
 import 'brace/theme/github';
+import 'brace/snippets/css'
+import 'brace/ext/language_tools'
 
 
 class CodeEditor extends Component {
   contructor() {
-    this.onChange = this.onChange.bind(this);
+    this.onChange = this.onChange.bind(this)
   }
 
   onChange(newValue, e) {
-    // const editor = this.ace.editor; // The editor object is from Ace's API
+    // const editor = this.ace.Editor; // The editor object is from Ace's API
     // console.log(editor.getValue()); // Outputs the value of the editor
   }
 
   render() {
     return (
       <AceEditor
-         mode="javascript"
+         mode="css"
          theme="github"
          onChange={this.onChange}
          name="UNIQUE_ID_OF_DIV"
-         editorProps={{$blockScrolling: true}}
+         editorProps={{$blockScrolling: 'Infinity'}}
+         enableBasicAutocompletion={true}
+         enableLiveAutocompletion={true}
+         enableSnippets={true}
       />
     )
   }
