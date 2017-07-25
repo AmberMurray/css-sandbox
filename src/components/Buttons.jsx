22b1388a6ef7951ad39515d6e97b7944762753d2
Amber Murray <amber.applebee@gmail.com>
2017-07-25 11:57:36 -0700

    adding styles to the library

--- src/components/Buttons.jsx
+++ src/components/Buttons.jsx
@@ -102,6 +102,8 @@ class Buttons extends Component {
           <select id="buttons" onChange={this.handleChange.bind(this)}>
             <option id="button-choice" value="try it out">Try It Out!</option>
             <option id='basic' value="basic">Basic</option>
+            <option id='backgroundImage' value="backgroundImage">Background Image</option>
+            <option id='border' value="border">Border</option>
             <option id='color' value="color">Color</option>
             <option id='rounded' value="rounded">Rounded</option>
           </select>
